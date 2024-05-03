0c49d16b9a474bf2b8e111c834e440118266e11c
Om Patil <ompatil220851@acropolis.in>
2024-05-03 22:18:52 +0530

    asyn

--- class-1/main.js
+++ class-1/main.js
@@ -1,1 +1,21 @@
- console.log("hello world")
+const fs = require("fs");
+
+fs.readFile("a.txt","utf-8",function(err,data){
+    console.log(data);
+})
+/* Asynchronous function in Js */
+
+console.log("hello 1") // hello 1 hello guys !!
+
+let a =0;
+for(let i =0 ; i<1000000;i++){
+    a++;
+}
+console.log("hello 2")
+
+/* Asynchronous functions
+fs module 
+setTimeout
+ */
+
+
