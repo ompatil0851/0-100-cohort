7826844bf547cb14b4e01b694d9325c355f5365e
Om Patil <ompatil220851@acropolis.in>
2024-05-04 16:30:16 +0530

    some style changes

--- class-1/main.js
+++ class-1/main.js
@@ -1,21 +1,19 @@
 const fs = require("fs");
 
-fs.readFile("a.txt","utf-8",function(err,data){
-    console.log(data);
-})
+fs.readFile("a.txt", "utf-8", function (err, data) {
+  console.log(data);
+});
 /* Asynchronous function in Js */
 
-console.log("hello 1") // hello 1 hello guys !!
+console.log("hello 1"); // hello 1 hello guys !!
 
-let a =0;
-for(let i =0 ; i<1000000;i++){
-    a++;
+let a = 0;
+for (let i = 0; i < 1000000; i++) {
+  a++;
 }
-console.log("hello 2")
+console.log("hello 2");
 
 /* Asynchronous functions
 fs module 
 setTimeout
  */
-
-
