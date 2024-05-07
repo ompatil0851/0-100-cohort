158020490b7f537196e4355073e320f096711179
Om Patil <ompatil220851@acropolis.in>
2024-05-07 22:27:18 +0530

    created a http server using express

--- class-2/http.js
+++ class-2/http.js
@@ -4,9 +4,31 @@ const express = require('express');
 const app = express();
 
 // Define a route that responds with "Hello, World!"
-app.get('/', function(req, res) {
-  res.send('Hello, World!');
+app.get('/homepage', function(req, res) {
+  res.send("Welcome to the homepage");
 });
+
+app.get('/page1', function(req, res) {
+  res.send("Welcome to the page1 ");
+});
+
+app.get('/page2', function(req, res) {
+  res.send("Welcome to the page2");
+});
+
+app.get('/page3', function(req, res) {
+  res.send("Welcome to the page3 ");
+});
+
+app.get('/page4', function(req, res) {
+  res.send("Welcome to the page4");
+});
+
+//used to direct the user if not on the correct route
+app.use(function(req, res, next) {
+  res.status(404).send("Sorry, that route doesn't exist. Have a nice day :)");
+});
+
 
 // Start the server on port 3000
 app.listen(3000, function(){
